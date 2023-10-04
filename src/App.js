e12a7ab20a8c48616a1e84fa14ca9032fcb06a7b
Paul Duncan <paul.duncan1984@gmail.com>
2023-10-04 14:45:50 +0700

    static setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialList = [
+	{
+		id: 1,
+		title: "Call bank",
+		description: "Close accounts",
+		due: "30/10/2023",
+		status: true,
+	},
+	{
+		id: 2,
+		title: "Go to gym",
+		description: "Leg day",
+		due: "02/10/2023",
+		status: true,
+	},
+	{
+		id: 1,
+		title: "Clean car",
+		description: "Inside and out",
+		due: "11/11/2023",
+		status: false,
+	},
+];
+
+function Button({ children }) {
+	return <button>{children}</button>;
 }
 
-export default App;
+export default function App() {
+	const [toDoList, setToDoList] = useState(initialList);
+
+	return (
+		<div>
+			<h1>To do list</h1>
+			<List toDoList={toDoList} />
+		</div>
+	);
+}
+
+function List({ toDoList }) {
+	return (
+		<div>
+			<ul>
+				{toDoList.map((item) =>
+					item.status ? <ListItem item={item} /> : null
+				)}
+			</ul>
+			<Button>Add item</Button>
+		</div>
+	);
+}
+
+function ListItem({ item }) {
+	// const [isActive, setIsActive] = useState(item);
+	return (
+		<div>
+			<li>
+				<input type="checkbox" />
+				<h2>{item.title}</h2>
+				<p>{item.description}</p>
+				<p>{item.due}</p>
+				<button>x</button>
+			</li>
+		</div>
+	);
+}
